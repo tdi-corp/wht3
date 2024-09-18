0134a8addfc00b2ee01bfc7ea88852d98c14eaa3
tdi-github <d1mmas28@gmail.com>
2024-09-18 19:32:08 +0300

    postcontroller - index, store, show

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -12,7 +12,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate(10);
+
+        return response()
+            ->json($posts);
     }
 
     /**
@@ -28,15 +31,36 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $query = Post::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'url1' => $request->url1,
+            'url2' => $request->url2,
+            'url3' => $request->url3,
+        ]);
+
+        $query->save();
+
+        return response()
+            ->json([
+                'id' => $query->id
+            ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Post $post, $id)
     {
-        //
+        $post_data = $post->query()->where('id', $id)->first();
+        
+        if(!isset($post_data)) {
+            abort(404);
+        }
+
+        return response()
+            ->json($post_data);
     }
 
     /**
