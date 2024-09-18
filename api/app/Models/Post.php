258dc60ca2e2c4f77bc78210a295d5450d980235
tdi-github <d1mmas28@gmail.com>
2024-09-18 20:56:29 +0300

    model fillable

--- api/app/Models/Post.php
+++ api/app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'description', 
+        'price', 
+        'url1',
+        'url2',
+        'url3',                 
+    ];    
 }
